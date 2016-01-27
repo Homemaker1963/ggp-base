6ce829625dc1cb3842503e0c20f8bcdae8eff59e
Steve Losh <steve@stevelosh.com>
2016-01-27 15:32:20 +0000

    First stab at a naive DFS

--- src/main/resources/playjure.clj
+++ src/main/resources/playjure.clj
@@ -1,11 +1,14 @@
 (ns gamer_namespace
-  (:require [clojure.tools.nrepl.server :as nrepl])
+  (:require [clojure.tools.nrepl.server :as nrepl]
+            [clojure.pprint :as pprint])
   (:import [org.ggp.base.player.gamer.statemachine StateMachineGamer]
            [org.ggp.base.util.statemachine.implementation.prover ProverStateMachine]))
+
 
 (def nrepl-server (ref nil))
 (def current-gamer (ref nil))
 
+; NREPL -----------------------------------------------------------------------
 (defn start-nrepl []
   (when (= (System/getenv "NREPL") "1")
     (println "Starting NREPL...")
@@ -14,35 +17,83 @@
         (ref-set nrepl-server (nrepl/start-server :port 7888))))))
 
 
+; DFS -------------------------------------------------------------------------
+(declare dfs)
+
+(defn is-terminal [state-machine current-state]
+  (.isTerminal state-machine current-state))
+
+(defn state-value [role state-machine current-state]
+  (.getGoal state-machine current-state role))
+
+(defn get-moves [role state-machine current-state]
+  (.getLegalMoves state-machine current-state role))
+
+(defn make-move [role state-machine current-state move]
+  (.getNextState state-machine current-state [move]))
+
+(defn find-best-move [role state-machine current-state]
+  (let [moves (get-moves role state-machine current-state)
+        results (map (fn [move]
+                       (dfs role state-machine
+                            (make-move role state-machine
+                                       current-state move)))
+                     moves)]
+    (apply max results)))
+
+
+(defn dfs [role state-machine current-state]
+  (if (is-terminal state-machine current-state) 
+    (state-value role state-machine current-state)
+    (find-best-move role state-machine current-state)))
+
+(defn run-dfs [role state-machine current-state]
+  (let [moves (get-moves role state-machine current-state)
+        results (map (fn [move]
+                       [(dfs role state-machine
+                             (make-move role state-machine
+                                        current-state move)) move])
+                     moves)]
+    (->> results
+      (sort (fn [[v1 _] [v2 _]]
+              (> v1 v2)))
+      first
+      second)))
+
+
+; Actual Player ---------------------------------------------------------------
+
+(defn start-game [gamer timeout]
+  (start-nrepl))
+
 (defn select-move [gamer timeout]
   (let [state-machine (.getStateMachine gamer)
         current-state (.getCurrentState gamer)
         role          (.getRole gamer)
-        random-move   (.getRandomMove state-machine
-                                      current-state
-                                      role)]
-    random-move))
+        move          (run-dfs role state-machine current-state)]
+    move))
+
+(defn stop-game [gamer])
+(defn abort-game [gamer])
 
 
 (defn Playjure []
   (dosync
-    (ref-set
-      current-gamer
-      (proxy [StateMachineGamer] []
-        (getInitialStateMachine []
-          (ProverStateMachine.))
+    (ref-set current-gamer
+             (proxy [StateMachineGamer] []
+               (getInitialStateMachine []
+                 (ProverStateMachine.))
 
-        (stateMachineSelectMove [timeout]
-          (select-move this timeout))
+               (stateMachineSelectMove [timeout]
+                 (select-move this timeout))
 
-        (stateMachineMetaGame [timeout]
-          (start-nrepl)
-          (println "Playjure metagame called"))
+               (stateMachineMetaGame [timeout]
+                 (start-game this timeout))
 
-        (stateMachineAbort []
-          (println "Playjure abort called"))
+               (stateMachineAbort []
+                 (abort-game this))
 
-        (stateMachineStop []
-          (println "Playjure stop called")))))
+               (stateMachineStop []
+                 (stop-game this)))))
   @current-gamer)
 
--- src/test/java/org/ggp/base/player/gamer/clojure/PlayjureGamerTest.java
+++ src/test/java/org/ggp/base/player/gamer/clojure/PlayjureGamerTest.java
@@ -1,29 +1,62 @@
 package org.ggp.base.player.gamer.clojure;
 
 import org.ggp.base.player.gamer.Gamer;
+import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.player.gamer.clojure.stubs.PlayjureStub;
-import org.ggp.base.util.game.GameRepository;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.LocalGameRepository;
 import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.match.Match;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.List;
+import java.util.Arrays;
 
 public class PlayjureGamerTest extends Assert {
     @Test
     public void testPlayjureGamer() {
         try {
             Gamer g = new PlayjureStub();
-            System.out.println("TEST");
 
             assertEquals("Playjure", g.getName());
 
-            Match m = new Match("", -1, 1000, 1000, GameRepository.getDefaultRepository().getGame("ticTacToe"), "");
+            LocalGameRepository repo = new LocalGameRepository();
+            Match m = new Match("", -1, 1000, 1000,
+                                repo.getGame("lights"), "");
+
             g.setMatch(m);
-            g.setRoleName(GdlPool.getConstant("xplayer"));
+            g.setRoleName(GdlPool.getConstant("you"));
             g.metaGame(1000);
-            assertTrue(g.selectMove(1000) != null);
+
+            StateMachine stateMachine;
+            MachineState currentState;
+            List<Move> previousMoves;
+
+            stateMachine = new ProverStateMachine();
+            stateMachine.initialize(m.getGame().getRules());
+            currentState = stateMachine.getInitialState();
+            previousMoves = null;
+            m.appendState(currentState.getContents());
+
+            java.util.List<Move> moves;
+            Move move;
+
+            while (!stateMachine.isTerminal(currentState)) {
+                move = new Move(g.selectMove(1000));
+                System.out.println("Player chose " + move.toString());
+
+                moves = java.util.Arrays.asList(move);
+                m.appendMoves2(moves);
+                currentState = stateMachine.getNextState(currentState, moves);
+                m.appendState(currentState.getContents());
+            }
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
+
 }
