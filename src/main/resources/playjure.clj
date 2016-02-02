27e245403e4185bc8952a31d5efe7b315c12d419
Steve Losh <steve@stevelosh.com>
2016-02-02 13:48:57 +0000

    Use an atom to hold the solution

--- src/main/resources/playjure.clj
+++ src/main/resources/playjure.clj
@@ -1,7 +1,7 @@
 (ns gamer_namespace
   (:require [clojure.tools.nrepl.server :as nrepl]
             [clojure.pprint :as pprint])
-  (:import 
+  (:import
     [org.ggp.base.util.statemachine StateMachine MachineState]
     [org.ggp.base.player.gamer.statemachine StateMachineGamer]
     [org.ggp.base.util.statemachine.implementation.prover ProverStateMachine]
@@ -9,81 +9,71 @@
 
 
 ; (set! *warn-on-reflection* true)
-(def nrepl-server (ref nil))
+(defonce nrepl-server
+  (when (= "1" (System/getenv "NREPL"))
+    (nrepl/start-server :port 7888)))
+
 (def current-gamer (ref nil))
 
-; NREPL -----------------------------------------------------------------------
-(defn start-nrepl []
-  (when (= (System/getenv "NREPL") "1")
-    (println "Starting NREPL...")
-    (dosync
-      (when-not @nrepl-server
-        (ref-set nrepl-server (nrepl/start-server :port 7888))))))
+(defrecord Node [role state-machine current-state])
+
+(def solution (atom [-1 []]))
+
+(defn swap-solution [[old-score _ :as old-solution]
+                     [new-score _ :as new-solution]]
+  (if (< old-score new-score)
+    new-solution
+    old-solution))
 
 
 ; DFS -------------------------------------------------------------------------
-(declare dfs)
 
-(defn is-terminal [^CachedStateMachine state-machine current-state]
+(defn is-terminal [{:keys [role state-machine current-state] :as node}]
   (.isTerminal state-machine current-state))
 
-(defn state-value [role ^CachedStateMachine state-machine current-state]
+(defn state-value [{:keys [role state-machine current-state] :as node}]
   (.getGoal state-machine current-state role))
 
-(defn get-moves [role ^CachedStateMachine state-machine current-state]
+(defn get-moves [{:keys [role state-machine current-state] :as node}]
   (.getLegalMoves state-machine current-state role))
 
-(defn make-move [role ^CachedStateMachine state-machine current-state move]
+(defn make-move [{:keys [role state-machine current-state] :as node} move]
   (.getNextState state-machine current-state [move]))
 
 
-(defn dfs-full [role ^CachedStateMachine state-machine current-state depth]
+(defn dfs-full [node path depth]
   (cond
-    (is-terminal state-machine current-state) 
-    [(state-value role state-machine current-state) (list)]
-
-    (zero? depth)
-    [-1 (list)]
+    (Thread/interrupted) nil
+    (is-terminal node) (dosync (swap! solution swap-solution
+                                      [(state-value node) path]))
+    (zero? depth) nil
 
     :else
-    (let [moves (get-moves role state-machine current-state)
-          next-depth (dec depth)]
-      (loop [best -1
-             best-path nil
-             move (first moves)
-             moves (rest moves)]
-        (let [next-state (make-move role state-machine current-state move)
-              [score path] (dfs-full role state-machine next-state next-depth)
-              [new-best new-path :as prev] (if (> best score)
-                                             [score path]
-                                             [best best-path])]
-          (cond
-            (= score 100) [100 (cons move path)]
-            (empty? moves) [new-best (cons move path)]
-            :else (recur new-best
-                         new-path
-                         (first moves)
-                         (rest moves))))))))
+    (dorun
+      (map (fn [move]
+             (dfs-full (assoc node :current-state (make-move node move))
+                       (conj path move)
+                       (dec depth)))
+           (get-moves node)))))
 
 
 ; Actual Player ---------------------------------------------------------------
-(def solution (ref nil))
-
 (defn start-game [^StateMachineGamer gamer timeout]
-  (let [state-machine (.getStateMachine gamer)
-        current-state (.getCurrentState gamer)
-        role          (.getRole gamer)
-        path          (loop [depth 1]
-                        (let [[score path] (dfs-full role state-machine current-state depth)]
-                          (if (pos? score)
-                            path
-                            (recur (inc depth)))))]
-    (dosync (ref-set solution path))))
+  (dosync (reset! solution [-1 []]))
+  (let [start-node (->Node (.getRole gamer)
+                           (.getStateMachine gamer)
+                           (.getCurrentState gamer))]
+    (loop [depth 1]
+      (dfs-full start-node [] depth)
+      (let [[score _] @solution]
+        (when-not (pos? score)
+          (recur (inc depth)))))))
 
 (defn select-move [gamer timeout]
   (dosync
-    (let [next-move (first @solution)]
-      (alter solution rest)
+    (let [[value path] @solution
+          [next-move & remaining-moves] path]
+      (reset! solution [value remaining-moves])
       next-move)))
 
 (defn stop-game [gamer])
